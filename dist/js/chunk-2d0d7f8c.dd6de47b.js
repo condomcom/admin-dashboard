e4f2498875ee0c358dda6fc079e7b15f39ab8ee3
Stanislav Ilin <youngfrezyx@gmail.com>
2021-09-20 00:07:49 +0500

    Configure production version for heroku

---
+++ dist/js/chunk-2d0d7f8c.dd6de47b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7f8c"],{"78a6":function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div")},a=[],r={name:"Conference"},u=r,o=c("2877"),s=Object(o["a"])(u,t,a,!1,null,"7eb05b1d",null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0d7f8c.dd6de47b.js.map
